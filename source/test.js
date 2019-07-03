f21130670bdb5aa1f54d8ada1d8787f74ed082ec
Demonyker <demonyker14@gmail.com>
2019-07-03 12:23:41 +0300

    m

--- source/test.js
+++ source/test.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const buttonNext = document.getElementsByClassName('button__next_arrow');
   const banners = document.getElementsByClassName('banner');
-  const buttonPrevious = document.getElementsByClassName('button__previous_arrow')
+  const buttonPrevious = document.getElementsByClassName('button__previous_arrow');
   for (let i = 0; i < buttonNext.length; i += 1) {
     buttonNext[i].onclick = () => {
       if (i + 1 !== buttonNext.length) {
