b7faa01e4f6b9f566d7b341f584ba020eb7b1f67
Demonyker <demonyker14@gmail.com>
2019-07-08 15:30:30 +0300

    testik

--- source/api.js
+++ source/api.js
@@ -1,13 +1,20 @@
 $(document).ready(() => {
   $.getJSON('https://baconipsum.com/api/?callback=?',
-    { 'type':'meat-and-filler', 'start-with-lorem': '1', 'paras': '3' },
-    (test) => {
-      if (test && test.length > 0) {
-        $('#baconIpsumOutput').html('');
-        for (let i = 0; i < test.length; i += 1) {
-          $('#baconIpsumOutput').append(`<p> + ${test[i]} + </p>`);
-          $('#baconIpsumOutput').show();
+    { 'type':'meat-and-filler', 'start-with-lorem': '1', 'paras': '5' },
+    (text) => {
+      if (text && text.length > 0) {
+        for (let i = 0; i < text.length; i += 1) {
+          $('.news').append(`<div class="news__block"> 
+          <div class="textline textline_purple"></div>
+          <div class="news__text">
+          <p>${text[i]}</p>
+          <p class="news__underline">Read more...</p>
+          </div> </div>`);
         }
       }
+    });
+  $.getJSON('https://picsum.photos/200',
+    (img) => {
+      console.log(img.url);
     });
 });
