2e9fb23d6381f87d0c83a63b58eab8be4b17956a
Demonyker <demonyker14@gmail.com>
2019-07-09 12:16:02 +0300

    mmmm

--- source/api.js
+++ source/api.js
@@ -1,20 +1,17 @@
 $(document).ready(() => {
   $.getJSON('https://baconipsum.com/api/?callback=?',
-    { 'type':'meat-and-filler', 'start-with-lorem': '1', 'paras': '5' },
+    { 'type':'meat-and-filler', 'start-with-lorem': '1', 'paras': '2' },
     (text) => {
       if (text && text.length > 0) {
         for (let i = 0; i < text.length; i += 1) {
           $('.news').append(`<div class="news__block"> 
           <div class="textline textline_purple"></div>
           <div class="news__text">
+          <div class="news__photo"><img src="https://picsum.photos/143/133?random=${i}"</div>
           <p>${text[i]}</p>
           <p class="news__underline">Read more...</p>
           </div> </div>`);
         }
       }
-    });
-  $.getJSON('https://picsum.photos/200',
-    (img) => {
-      console.log(JSON.parse(img));
     });
 });
