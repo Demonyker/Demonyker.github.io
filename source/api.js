12d834f1d59a0bfe841a4027a3283eee2404ffcd
Demonyker <demonyker14@gmail.com>
2019-07-10 11:35:49 +0300

    zapros

--- source/api.js
+++ source/api.js
@@ -1,16 +1,17 @@
 $(document).ready(() => {
   $.getJSON('https://baconipsum.com/api/?callback=?',
-    { 'type':'meat-and-filler', 'start-with-lorem': '1', 'paras': '2' },
+    { 'type':'meat-and-filler', 'start-with-lorem': '1', 'paras': '4' },
     (text) => {
       if (text && text.length > 0) {
         for (let i = 0; i < text.length; i += 1) {
           $('.news').append(`<div class="news__block"> 
-          <div class="textline textline_purple"></div>
+          <div class="textline textline_purple"><span class="textline__text">ABOUT SUPER LOGO</span></div>
           <div class="news__text">
-          <div class="news__photo"><img src="https://picsum.photos/143/133?random=${i}"</div>
-          <p>${text[i]}</p>
+          <img class="news__photo"src="https://picsum.photos/143/133?random=${i}">
+          <div class="news__content">
+          <p class="news__content_text">${text[i]}</p>
           <p class="news__underline">Read more...</p>
-          </div> </div>`);
+          </div> </div> </div>`);
         }
       }
     });
