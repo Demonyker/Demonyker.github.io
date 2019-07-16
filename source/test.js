497253071f6c01e4a306c64bcfc1be96b34d4855
Demonyker <demonyker14@gmail.com>
2019-07-16 14:51:22 +0300

    posts

--- source/test.js
+++ source/test.js
@@ -33,21 +33,36 @@ window.onload = () => {
       xhr.onerror = () => {
         reject(new Error('Nerwork error'));
       };
-      xhr.open('GET', 'https://baconipsum.com/api/?callback=?type=meat-and-filler&start-with-lorem=1&paras=3');
+      xhr.open('GET', 'https://baconipsum.com/api/?callback=?type=meat-and-filler&start-with-lorem=1&paras=4');
       xhr.send();
     });
   }
-  promiseAjax().then((jsonString) => {
-    const massiveOftext = JSON.parse(jsonString);
+  function kartinki() {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = () => {
+        resolve(JSON.parse(xhr.responseText));
+      };
+      xhr.open('GET', 'https://picsum.photos/v2/list?page=2&limit=4');
+      xhr.send();
+    });
+  }
+  Promise.all([promiseAjax(), kartinki()]).then((results) => {
+    const posts = [];
+    const massiveOftext = JSON.parse(results[0]);
+    const photos = results[1];
     massiveOftext.forEach((text, i) => {
-      news.innerHTML =`<div class="news__block"> 
+      posts.push({ text: text, img: photos[i].download_url });
+    });
+    posts.forEach((post, i) => {
+      news.innerHTML = `<div class="news__block"> 
           <div class="textline textline_purple"><span class="textline__text">ABOUT SUPER LOGO</span></div>
           <div class="news__text">
-          <img class="news__photo"src="https://picsum.photos/143/133?random=${i}">
+          <img class="news__photo"src="${post.img}">
           <div class="news__content">
-          <p class="news__content_text">${text}</p>
+          <p class="news__content_text">${post.text}</p>
           <p class="news__underline">Read more...</p>
-          </div> </div> </div>` + news.innerHTML;
+          </div> </div> </div>${news.innerHTML}`;
     });
     const newsBlocks = document.getElementsByClassName('news__block');
     if (newsBlocks.length % 2 !== 0) {
