3e22a0525e7f71727bc5a4320a4463871e1c2b75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-02 09:43:09 +0000

    Deploy Demonyker/Frontend to github.com/Demonyker/Frontend.git:gh-pages

---
+++ source/test.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  const buttonNext = document.getElementsByClassName('button__next_arrow');
+  const banners = document.getElementsByClassName('banner');
+  const buttonPrevious = document.getElementsByClassName('button__previous_arrow');
+  for (let i = 0; i < buttonNext.length; i += 1) {
+    buttonNext[i].onclick = () => {
+      if (i + 1 !== buttonNext.length) {
+        banners[i].style.display = 'none';
+        banners[i + 1].style.display = 'flex';
+      }
+    };
+  }
+  for (let i = 0; i < buttonPrevious.length; i += 1) {
+    buttonPrevious[i].onclick = () => {
+      if (i - 1 !== -1) {
+        banners[i].style.display = 'none';
+        banners[i - 1].style.display = 'flex';
+      }
+    };
+  }
+};
